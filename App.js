0a204abe9e133dc0161ada45ececdb0e99a85bff
Gagandeep Singh <gagan@homedruid.com>
2020-02-21 12:46:46 +0530

    Removing the state logic from App. It moves into the splash login page

--- App.js
+++ App.js
@@ -7,8 +7,6 @@
 
 import React from 'react';
 import {initFirebase} from './moduleSrc/platform/firebase.native';
-import {setupInternalState} from './moduleSrc/router/InternalState.native';
-import {store} from './moduleSrc/router/store';
 import {App as Application} from './moduleSrc/router/Flow.native';
 import cnsole from 'loglevel';
 import {setPushyNotificationListeners} from './src/util/pushy';
@@ -19,7 +17,6 @@ cnsole.info('****** App starting ********', new Date().getTime());
 
 setPushyNotificationListeners();
 initFirebase();
-setupInternalState(store);
 
 export {
     Application,
