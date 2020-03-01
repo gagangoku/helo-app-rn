4634aad1c7e04f6a8ed61f125cfd9b573cca14d2
Gagandeep Singh <gagan@homedruid.com>
2020-03-01 23:36:38 +0530

    New release

--- App2.js
+++ App2.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import {View} from 'react-native';
-import {Spreadsheet} from './moduleSrc/platform/Spreadsheet.native';
 import cnsole from 'loglevel';
 import {ExcelDemo} from './src/demos/ExcelDemo';
+import ChatDemo from './moduleSrc/demos/ChatDemo';
+import {setupInternalStateFromLocal} from './moduleSrc/router/InternalState.native';
+import {store} from './moduleSrc/router/store';
 
 
 cnsole.setLevel('info');
 cnsole.info('****** Test app ********', new Date().getTime());
+setupInternalStateFromLocal(store);
 
-export class Application1 extends React.PureComponent {
+export class Application extends React.PureComponent {
     submitFn = (x) => {
         console.log('spreadsheet: ', x);
     };
     render() {
         return (
-            <View style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
-                <View style={{ height: '80%', width: '80%' }}>
-                    <Spreadsheet submitFn={this.submitFn} isEditable={true} initialRows={[[], [1, 2, 3], ['', 5, 6]]}
-                                 numRows={7} numCols={8} rowHeader={['r1', 'r2', 'r3', 'r4', 'r5', 'r6']} />
-                </View>
+            <View style={{ height: '100%', width: '100%' }}>
+                <ChatDemo />
             </View>
         );
     }
 }
-export class Application extends React.PureComponent {
+export class Application1 extends React.PureComponent {
     submitFn = (x) => {
         console.log('spreadsheet: ', x);
     };
--- src/demos/ExcelDemo.js
+++ src/demos/ExcelDemo.js
@@ -3,6 +3,8 @@ import {OUTPUT_EXCEL, OUTPUT_TEXT, SENDER_VISITOR} from '../../moduleSrc/chat/Qu
 import {COOK_ONBOARDING_FLOW, getChatContext} from '../../moduleSrc/chat/bot/ChatUtil';
 import {HELO_LOGO, MODE_GROUP_CHAT} from '../../moduleSrc/chat/Constants';
 import MessagingUI from '../../moduleSrc/chat/messaging/MessagingUI';
+import {firebase} from '../../moduleSrc/platform/firebase';
+import {FIREBASE_GROUPS_DB_NAME} from '../../moduleSrc/constants/Constants';
 
 
 export class ExcelDemo extends React.Component {
@@ -27,14 +29,25 @@ export class ExcelDemo extends React.Component {
     onUserMsg = async ({ text, type, ...extra }) => {
         console.log('onUserMsg: ', text, type, extra);
         const messages = this.state.messages.slice();
-        messages.push({
+
+        const newMsgPayload = {
             timestamp: new Date().getTime(),
             text,
             type,
             sender: SENDER_ME,
             ...extra,
-        });
+        };
+        messages.push(newMsgPayload);
         this.setState({ messages });
+
+        // console.log('Saving to firebase');
+        // const db = firebase.firestore();
+        // const docRef = db.collection(FIREBASE_GROUPS_DB_NAME).doc('helo-kitchen-indiranagar');
+        //
+        // await docRef.update({
+        //     messages: firebase.firestore.FieldValue.arrayUnion(newMsgPayload),
+        // });
+        // console.log('Saved');
     };
 
     render() {
@@ -79,7 +92,65 @@ export class ExcelDemo extends React.Component {
     }
 }
 
-const SENDER_ME = 'me';
+const items =
+`Butter
+Cheesy Crispy Chicken Fries
+Chicken Pop Corn
+Chicken Wings
+Cooking Cream
+French Fries Sure Crisp
+Garlic pops
+Jalapeno Popper
+Potato Wedges
+Ripple Icecream-2 Bulk
+Whipped Cream
+Basa Fish
+Chicken Breast
+Mutton Boneless
+Salami Chicken
+BATATA VADA
+BHUNA CHICKEN
+CHICKEN SHAMI KEBAB
+CHICKEN TIKKA
+CHILLY PANEER
+FALAFEL
+Green Peas in
+LACCHA PARATHA  ORG
+MINI PARATHA
+MIXED VEG FILLING
+PANEER TIKKA
+SZECHUAN CHICKEN
+VEG FINGER
+WHOLE WHEAT PARATHA
+BACON SLICE
+Cheddar Cheese
+Paneer
+Sweet Corn
+Amul mozrella diced
+Cheese Slice
+Green Peace`.split('\n');
+
+const initialRows = {
+    0: { 0: '', 1: '', 2: '' },
+};
+items.forEach((item, idx) => {
+    initialRows[idx+1] = {
+        0: (idx+1) + '.',
+        1: item,
+    };
+});
+const excel = {
+    title: 'Inventory',
+    isEditable: true,
+    initialRows,
+    rowHeader: ['No.', 'Item', 'Current stock', 'Notes'],
+    rowStyles: [{}, { width: 150 }, { width: 80 }, { width: 80 }],
+    cellHeight: 60,
+    cellWidth: 50,
+    numRows: items.length+3, numCols: 4,
+};
+
+const SENDER_ME = 'supply:352';
 const INITIAL_MESSAGES2 = [{
     type: OUTPUT_TEXT,
     text: 'Hello',
@@ -90,18 +161,5 @@ const INITIAL_MESSAGES2 = [{
     sender: SENDER_VISITOR,
     text: 'Diet chart',
     timestamp: new Date().getTime(),
-    excel: {
-        title: 'diet chart',
-        isEditable: true,
-        initialRows: {
-            0: {},
-            1: { 0: 'Salt', 1: '100 gm' },
-            2: { 0: 'Sugar', 1: '200 gm' },
-            3: { 0: 'Protein', 1: '400 gm' },
-        },
-        rowHeader: ['Item', 'Quantity'],
-        cellHeight: 40,
-        cellWidth: 80,
-        numCols: 4, numRows: 4,
-    },
+    excel,
 }];
