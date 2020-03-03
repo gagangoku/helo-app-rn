1c29ec90b6a2f12988525a28df1205af3d449459
Gagandeep Singh <gagan@homedruid.com>
2020-03-03 14:05:21 +0530

    firebase background

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 import {AppRegistry} from 'react-native';
 import {Application} from './App';
 import {name as appName} from './app.json';
-// import {handleFirebaseMessage} from './src/util/firebase';
+import {handleFirebaseMessage} from './moduleSrc/platform/firebase.native';
 
 
 AppRegistry.registerComponent(appName, () => Application);
-// AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => handleFirebaseMessage);
-// AppRegistry.registerHeadlessTask('RNFirebaseMessaging', () => handleFirebaseMessage);
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => handleFirebaseMessage);
+AppRegistry.registerHeadlessTask('RNFirebaseMessaging', () => handleFirebaseMessage);
