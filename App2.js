09c62dbbce673bb36becb648a402b0c3d37604be
Gagandeep Singh <gagan@homedruid.com>
2020-03-12 18:19:07 +0530

    Task list working

--- App2.js
+++ App2.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import cnsole from 'loglevel';
 import {ExcelDemo} from './src/demos/ExcelDemo';
 import ChatDemo from './moduleSrc/demos/ChatDemo';
 // import Promise from 'promise';
 import EditableTextBox from './moduleSrc/widgets/EditableTextBox';
 import {FlatbufferDemo} from './moduleSrc/demos/FlatbufferDemo';
+import {Modal, WINDOW_INNER_HEIGHT, WINDOW_INNER_WIDTH} from './moduleSrc/platform/Util';
+import {TaskList} from './moduleSrc/platform/TaskList';
+import {noOpFn} from './moduleSrc/util/Util';
+import TouchableAnim from './moduleSrc/platform/TouchableAnim';
 
 
 cnsole.setLevel('info');
@@ -67,6 +71,48 @@ class ApplicationFbs extends React.PureComponent {
     }
 }
 
+class ApplicationFSModal extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalOpen: false,
+        };
+    }
+    render () {
+        const { modalOpen } = this.state;
+        const modalStyle = {};
+        const me = {};
+        const sender = {};
+        const payload = {
+            tasks: [],
+            title: 'Hello',
+        };
+
+        const closeFn = () => this.setState({ modalOpen: false });
+        const submitFn = noOpFn;
+
+        return (
+            <View>
+                <TouchableAnim onPress={() => this.setState({ modalOpen: true })}>
+                    <Text>Hi</Text>
+                </TouchableAnim>
+                {modalOpen && <Modal isOpen={modalOpen} visible={modalOpen} isVisible={modalOpen}
+                                     backdropOpacity={0.5} style={modalStyle}
+                                     onRequestClose={closeFn} onBackdropPress={closeFn}
+                                     onAfterOpen={() => {}} contentLabel="Example Modal">
+                    <View style={{ height: '100%', width: '100%', backgroundColor: 'white',
+                                   display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={{ height: WINDOW_INNER_HEIGHT, width: WINDOW_INNER_WIDTH,
+                            display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                            <TaskList me={me} sender={sender} payload={payload} isPreview={false} closeFn={closeFn} submitFn={submitFn} />
+                        </View>
+                    </View>
+                </Modal>}
+            </View>
+        );
+    }
+}
+
 export {
-    ApplicationFbs as Application,
+    ApplicationFSModal as Application,
 }
