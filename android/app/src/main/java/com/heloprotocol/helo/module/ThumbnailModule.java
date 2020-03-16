419d9704482b79a3a1150a1b1f446c5714f3136d
Gagandeep Singh <gagan@homedruid.com>
2020-03-16 18:18:01 +0530

    Move video thumbnail creation to background thread

--- android/app/src/main/java/com/heloprotocol/helo/module/ThumbnailModule.java
+++ android/app/src/main/java/com/heloprotocol/helo/module/ThumbnailModule.java
@@ -1,33 +1,22 @@
 package com.heloprotocol.helo.module;
 
-import android.widget.Toast;
-
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import android.media.ThumbnailUtils;
 import android.graphics.Bitmap;
-import android.provider.MediaStore;
-import com.facebook.react.bridge.Callback;
-import java.io.ByteArrayOutputStream;
-import android.util.Base64;
 import android.media.MediaMetadataRetriever;
+import android.os.AsyncTask;
 import android.os.Build;
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.Promise;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+import com.facebook.react.bridge.*;
 
-import java.util.Map;
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
-import java.lang.Exception;
+import java.util.Map;
+
 
 public class ThumbnailModule extends ReactContextBaseJavaModule {
     private static ReactApplicationContext reactContext;
-
-    private static final String DURATION_SHORT_KEY = "SHORT";
-    private static final String DURATION_LONG_KEY = "LONG";
+    private static final String TAG = "ReactNative.ThumbnailModule";
 
     ThumbnailModule(ReactApplicationContext context) {
         super(context);
@@ -42,8 +31,6 @@ public class ThumbnailModule extends ReactContextBaseJavaModule {
     @Override
     public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
-        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
-        constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
         return constants;
     }
 
@@ -54,35 +41,10 @@ public class ThumbnailModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void createVideoThumbnail(String filePath, int maxWidth, int maxHeight, int quality, Promise promise) {
-        Bitmap bitmap;
-        String time;
-        try {
-            Map<String, Object> map = retriveVideoFrameFromVideo(filePath);
-            bitmap = (Bitmap) map.get("bitmap");
-            time = (String) map.get("time");
-        } catch (Exception e) {
-            promise.reject("Failed", e);
-            return;
-        }
-
-        int h = bitmap.getHeight();
-        int w = bitmap.getWidth();
-        float scale1 = (float) maxHeight / (float) h;
-        float scale2 = (float) maxWidth  / (float) w;
-        float scale = Math.min(scale1, scale2);
-        bitmap = Bitmap.createScaledBitmap(bitmap, (int) Math.floor(w * scale), (int) Math.floor(h * scale), false);
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
-        byte[] byteArray = baos.toByteArray();
-        String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
-
-        WritableMap map = Arguments.createMap();
-        map.putString("encoded", encoded);
-        map.putInt("width", bitmap.getWidth());
-        map.putInt("height", bitmap.getHeight());
-        map.putString("time", time);
-        promise.resolve(map);
+        Log.i(TAG, "createVideoThumbnail started");
+        AsyncTask asyncTask = new ThumbnailTask(filePath, maxWidth, maxHeight, quality, promise);
+        asyncTask.execute(new String[] {});
+        Log.i(TAG, "createVideoThumbnail finished");
     }
 
     public static Map<String, Object> retriveVideoFrameFromVideo(String videoPath) {
@@ -106,6 +68,73 @@ public class ThumbnailModule extends ReactContextBaseJavaModule {
             if (retriever != null) {
                 retriever.release();
             }
+        }
+    }
+
+    private class ThumbnailTask extends AsyncTask<String, String, String> {
+        private String filePath;
+        private int maxWidth;
+        private int maxHeight;
+        private int quality;
+        private Promise promise;
+        public ThumbnailTask(String filePath, int maxWidth, int maxHeight, int quality, Promise promise) {
+            super();
+            this.filePath = filePath;
+            this.maxWidth = maxWidth;
+            this.maxHeight = maxHeight;
+            this.quality = quality;
+            this.promise = promise;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... x) {
+            Log.i(TAG, "createVideoThumbnail doInBackground");
+            createThumbnail(this.filePath, this.maxWidth, this.maxHeight, this.quality, this.promise);
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String bitmap) {
+            super.onPostExecute(bitmap);
+        }
+
+        public void createThumbnail(String filePath, int maxWidth, int maxHeight, int quality, Promise promise) {
+            Log.i(TAG, "createThumbnail started");
+            Bitmap bitmap;
+            String time;
+            try {
+                Map<String, Object> map = ThumbnailModule.retriveVideoFrameFromVideo(filePath);
+                bitmap = (Bitmap) map.get("bitmap");
+                time = (String) map.get("time");
+            } catch (Exception e) {
+                promise.reject("Failed", e);
+                return;
+            }
+
+            int h = bitmap.getHeight();
+            int w = bitmap.getWidth();
+            float scale1 = (float) maxHeight / (float) h;
+            float scale2 = (float) maxWidth  / (float) w;
+            float scale = Math.min(scale1, scale2);
+            bitmap = Bitmap.createScaledBitmap(bitmap, (int) Math.floor(w * scale), (int) Math.floor(h * scale), false);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
+            byte[] byteArray = baos.toByteArray();
+            String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+            WritableMap map = Arguments.createMap();
+            map.putString("encoded", encoded);
+            map.putInt("width", bitmap.getWidth());
+            map.putInt("height", bitmap.getHeight());
+            map.putString("time", time);
+            promise.resolve(map);
+            Log.i(TAG, "createThumbnail finished");
         }
     }
 }
